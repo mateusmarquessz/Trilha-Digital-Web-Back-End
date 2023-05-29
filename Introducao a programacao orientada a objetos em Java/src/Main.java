f3ceab038484d4a0ca65577af2e86b249013e5cf
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-05-28 21:20:12 -0300

    Att

---
+++ Introducao a programacao orientada a objetos em Java/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+
+        byte variavelByte = 127; // maximo que byte aguenta
+        //short - 32768 a 32767
+        //char 0 a 65535
+        //int -2b a 2b
+        //long -9t a 9t
+
+        int variavelInt = 2147483;
+        System.out.println(variavelInt);
+    }
+}
