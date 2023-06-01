29e9adddd359088fa0f6bfb0cc46cc3685e0fe16
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-06-01 20:37:54 -0300

    att

--- Atividade de POO/src/Animais/Cachorro.java
+++ Atividade de POO/src/Animais/Cachorro.java
@@ -1,10 +1,36 @@
 package Animais;
-
+//atributos  =  caracteristiscas sobre a class
+//Atributos tbm sao as definições desse atratibutos
 public class Cachorro {
 
-    String nome;
-    String cor;
-    double altura;
-    double peso;
-    int tamanhoDoRabo;
+
+    //Atributos que a class Cachorro
+    public String nome;
+    public String cor;
+    public double altura;
+    public double peso;
+    public int tamanhoDoRabo;
+
+    public String estadoDeEspirito;
+
+    //Metodos e equivalente a uma Funcao
+
+    public void comer(){}
+
+    public void latir(){
+        System.out.println("Au Au");
+    }
+
+   public String pegar(){
+        return "Bolinha";
+    }
+
+    public String interagir(String acao){
+        if (acao.equals("carinho")){ //equals compara Strings
+            this.estadoDeEspirito = "feliz"; //This = Esse
+        } else {
+            this.estadoDeEspirito = "Neutro";
+        }
+        return estadoDeEspirito;
+    }
 }
--- Atividade de POO/src/Main.java
+++ Atividade de POO/src/Main.java
@@ -1,9 +1,29 @@
 
 //Classe = A classe define como serao as coisas, como a casa vai ser! Definicao
-//Objeto = E a casa e as caracteriscas e etc
+//Objeto = E a casa e as caracteriscas e e
+
+import Animais.Cachorro;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Variavel de referencia, funciona como referencia do objeto da class Cachorro
+
+
+        //Criando Objeto na memoria
+        //Define a Class cria o nome da variavel de referencia e cria o novo Objeto
+        Cachorro cachorro1 = new Cachorro();
+
+        //Puxando varivel da class Cachorro, usando a variavel de referencia
+        //Definindo na memoria um OBJETO
+        cachorro1.nome = "Puppy";
+        cachorro1.cor = "Preto";
+        cachorro1.altura = 25.4;
+        cachorro1.peso = 10.5;
+        cachorro1.tamanhoDoRabo = 5;
+
+        cachorro1.latir();
+        System.out.println("O cachorro pegou uma " + cachorro1.pegar());
+
+        System.out.println("O cachorro esta " + cachorro1.interagir("carinho"));
     }
 }
